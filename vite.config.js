b94b95c9d9f1fc4c2076d5a17b1eb3f2c0039b7c
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-06-18 10:09:28 +0700

    chore: setup icons

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,20 @@ import oxfmt from "@kekkon-nexus/config/oxfmt";
 import oxlint from "@kekkon-nexus/config/oxlint";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import icons from "unplugin-icons/vite";
 import { defineConfig } from "vite-plus";
 
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [
+		react(),
+		tailwindcss(),
+		icons({
+			compiler: "jsx",
+			jsx: "react",
+			scale: 1,
+			defaultClass: "*:stroke-1",
+		}),
+	],
 
 	fmt: {
 		...oxfmt,
